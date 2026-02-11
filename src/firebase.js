a6d056dcd4037f9fea5470d9637fab14999343eb
aryafian <aryafian232@gmail.com>
2026-02-11 19:50:45 +0700

    set firebase

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyCrlg5ewCFfJXNljmArVMiw6X2BH-iItyg",
+  authDomain: "green-mini-soccer.firebaseapp.com",
+  projectId: "green-mini-soccer",
+  storageBucket: "green-mini-soccer.firebasestorage.app",
+  messagingSenderId: "521115529368",
+  appId: "1:521115529368:web:400b630831e4d71b2ce0ed",
+  measurementId: "G-W29Y91KN8N"
 };
 
 // Initialize Firebase
